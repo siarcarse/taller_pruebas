09de21b29f0f061a77e4735a205d16b6a145a8e9
Simplicio Carcamo <siarcarse@gmail.com>
2017-11-30 21:25:45 -0300

    Clase 25-11-2017

--- public/app/users/main.js
+++ public/app/users/main.js
@@ -1,33 +1,43 @@
 $(document).ready(function() {
-    	console.log('Fuera del ajax arriba'); 
-        $.ajax({
-                url: '/api/users',
-                type: 'GET',
-                dataType: 'json'
-            })
-            .done(function(users) {
-            	console.log('Dentro del ajax');
-                if(users) {
-                    var html = '';
-                	for (var i = 0; i < users.length; i++) {
-                		var usersObj = users[i];
-                		html += '<tr>';
-                		html += '<td>' + usersObj.id + '</td>';
-                		html += '<td>' + usersObj.username + '</td>';
-                		html += '<td>' + usersObj.password + '</td>';
-                		html += '</tr>';
-                	}
-                	$('#body-table').html(html);
-                }
-            })
-            .fail(function(err) {
-                console.log(err);
-            });
-            $('#saveButton').click(function(event) {
-                var username = $('#username').val();
-                var password = $('#password').val();
-                $.post('/api/users', {username: username, password: password}, function(data, textStatus, xhr) {
-                    window.location.reload(true);
-                });
-            });
+    refreshTable();
+    $('#saveButton').click(function(event) {
+        var username = $('#username').val();
+        var password = $('#password').val();
+        $.post('/api/users', { username: username, password: password }, function(data, textStatus, xhr) {
+            refreshTable();
+            $('#modal-id').modal('hide');
+        });
+        $('#borrar').click(function(event) {
+            console.log('me hicieron click'); 
+        });
     });
+});
+
+function refreshTable() {
+    $.ajax({
+            url: '/api/users',
+            type: 'GET',
+            dataType: 'json'
+        })
+        .done(function(users) {
+            if (users) {
+                var html = '';
+                for (var i = 0; i < users.length; i++) {
+                    var usersObj = users[i];
+                    html += '<tr>';
+                    html += '<td>' + usersObj.id + '</td>';
+                    html += '<td>' + usersObj.username + '</td>';
+                    html += '<td>' + usersObj.password + '</td>';
+                    html += '<td><i style="color: #FC57FB; cursor: pointer;" class="fa fa-ban fa-2x" aria-hidden="true" onclick="deleteRow(this)"></i></td>';
+                    html += '</tr>';
+                }
+                $('#body-table').html(html);
+            }
+        })
+        .fail(function(err) {
+            console.log(err);
+        });
+}
+function deleteRow(icono) {
+    var id = $(icono).parent().siblings().first().text();
+}
